ffa7d30d9711f521d5070947d608a325c521a37e
shaos91@126.com <shaos91@126.com>
2022-07-21 14:44:48 +0800

    refactor: 组件重命名

---
+++ springboot-rabbitmq-v1/src/main/java/com/example/demo/RabbitMQV1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitMQV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitMQV1Application.class, args);
+	}
+
+}
